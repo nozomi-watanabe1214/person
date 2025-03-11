7864de3131e6cb5757f457b3054689d6124f7b29
渡辺のぞみ <@192.168.0.15>
2025-03-11 23:01:08 +0900

    Javaオブジェクト指向（演習②完了）

--- src/person/Person.java
+++ src/person/Person.java
@@ -4,5 +4,7 @@ public class Person {
 
 	public String name = null;
 	public int age = 0;
-	}
+	
+		}
+	
 
--- src/person/Test.java
+++ src/person/Test.java
@@ -6,12 +6,28 @@ public class Test {
 		// TODO 自動生成されたメソッド・スタブ
 		
 		Person taro = new Person();
+		Person jiro = new Person();
+		Person hanako = new Person();
+		Person nozomi = new Person();
+		
 		taro.name = "山田太郎";
+		jiro.name = "木村次郎";
+		hanako.name = "鈴木花子";
+		nozomi.name = "渡辺のぞみ";
+		
 		taro.age = 20;
+		jiro.age = 18;
+		hanako.age = 16;
+		nozomi.age = 28;
 		
 		System.out.println(taro.name);
 		System.out.println(taro.age);
-
-	}
-
+		System.out.println(jiro.name);
+		System.out.println(jiro.age);
+		System.out.println(hanako.name);
+		System.out.println(hanako.age);
+		System.out.println(nozomi.name);
+		System.out.println(nozomi.age);
+		
+}
 }
