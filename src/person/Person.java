d7854a1a179229f8678f6f4fd96f905de0679327
渡辺のぞみ <@10.41.253.225>
2025-03-12 21:47:19 +0900

    Javaオブジェクト指向（演習③完了）

--- src/person/Person.java
+++ src/person/Person.java
@@ -4,9 +4,11 @@ public class Person {
 
 	public String name = null;
 	public int age = 0;
+	public String phoneNumber = null;
+	public String address = null;
 	
 	public void person() {
-		System.out.println(this.name + this.age);
+		System.out.println(this.name + this.age + this.phoneNumber + this.address);
 	}
 		}
 	
