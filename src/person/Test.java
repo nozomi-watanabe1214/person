04615a7c1fb80b331e611ce356e5a6d4394a04fb
渡辺のぞみ <@10.41.177.117>
2025-03-13 21:07:25 +0900

    Javaオブジェクト指向（演習⑧完了）

--- src/person/Test.java
+++ src/person/Test.java
@@ -5,35 +5,23 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		
-		Person taro = new Person();
-		Person jiro = new Person();
-		Person hanako = new Person();
-		Person nozomi = new Person();
+		Robot  aibo = new Robot();
+		Robot asimo = new Robot();
+		Robot pepper = new Robot();
 		
-		taro.name = "山田太郎";
-		jiro.name = "木村次郎";
-		hanako.name = "鈴木花子";
-		nozomi.name = "渡辺のぞみ";
+		aibo.name = "aibo";
+		asimo.name = "asimo";
+		pepper.name = "pepper";
 		
-		taro.age = 20;
-		jiro.age = 18;
-		hanako.age = 16;
-		nozomi.age = 28;
-		
-		taro.phoneNumber = "080-1234-5678";
-		jiro.phoneNumber = "070-5678-1234";
-		hanako.phoneNumber = "050-1234-5678";
-		nozomi.phoneNumber = "090-5678-1234";
-		
-		taro.address = "東京";
-		jiro.address = "埼玉";
-		hanako.address = "千葉";
-		nozomi.address = "神奈川";
-		
-		taro.jouhou();
-		taro.talk();
-		taro.walk();
-		taro.run();
+		aibo.talk();
+		asimo.talk();
+		pepper.talk();
+		aibo.walk();
+		asimo.walk();
+		pepper.walk();
+		aibo.run();
+		asimo.run();
+		pepper.run();
 		}
 	}
 		
